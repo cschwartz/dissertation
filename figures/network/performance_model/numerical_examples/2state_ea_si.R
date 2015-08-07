22ca42c1e7118410a8472fe5832f85d1855663e4
Christian Schwartz <christian.schwartz@gmail.com>
2015-08-07 16:17:06 +0200

    added si notation for figure

--- figures/network/performance_model/numerical_examples/2state_ea_si.R
+++ figures/network/performance_model/numerical_examples/2state_ea_si.R
@@ -2,6 +2,18 @@ data <- read.csv('2state_ea_si.csv', sep=',', comment.char='#')
 
 data$cA <- factor(data$cA)
 
+fancy_scientific <- function(l) {
+  # turn in to character string in scientific notation
+  l <- format(l, scientific = TRUE)
+  # quote the part before the exponent to keep all the digits
+  l <- gsub("^(.*)e", "'\\1'e", l)
+  # turn the 'e+' into plotmath format
+  l <- gsub("e", "%*%10^", l)
+  # return this as an expression
+  parse(text=l)
+}
+
+
 p <- ggplot(data, aes(x = EA, 
                       y = SI, 
                       color = cA)) + 
@@ -10,6 +22,7 @@ p <- ggplot(data, aes(x = EA,
   scale_color_manual(values = color.palette) +
   labs(x = label.ea, 
        y = label.signalling.intensity, 
-       color = label.cA )
+       color = label.cA ) +
+  scale_y_continuous(labels = fancy_scientific)
 
 save.full.row.plot(p)
