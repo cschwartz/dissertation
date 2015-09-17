93fb7114c1d3863a18561eb48dfb7ef4f9000a1e
christian.schwartz@gmail.com <christian.schwartz@gmail.com>
2015-09-17 17:49:38 +0200

    added text for 3.3.1

---
+++ figures/application/lte_video/system_model/abort_distributions.R
@@ -0,0 +1,14 @@
+data <- read.csv('abort_distributions.csv')
+
+p <- ggplot(data, aes(x = time,
+                      y = value,
+                      color = variable)) +
+  geom_line() +
+  labs(x = label.abort.time,
+       y = label.pdf.abort.time,
+       color = label.user.model) +
+  scale_x_continuous(breaks = seq(0, 1600, 400)) +
+  scale_colour_manual(values = color.palette)
+
+save.full.row.plot(p)
+
