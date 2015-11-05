202f09e5c12fad336e0ee8b3410b77b08418afef
Christian Schwartz <christian.schwartz@gmail.com>
2015-11-05 17:21:11 +0100

    stuff

--- figures/application/lte_video/system_model/abort_distributions.R
+++ figures/application/lte_video/system_model/abort_distributions.R
@@ -7,8 +7,8 @@ p <- ggplot(data, aes(x = time,
   labs(x = label.abort.time,
        y = label.pdf.abort.time,
        color = label.user.model) +
+  scale_y_continuous(breaks = seq(0, 1e-3, length.out=5), labels = notation.si) +
   scale_x_continuous(breaks = seq(0, 1600, 400)) +
   scale_colour_manual(values = color.palette)
 
 save.full.row.plot(p)
-
--- figures/application/lte_video/trade_offs/energy2lostData.R
+++ figures/application/lte_video/trade_offs/energy2lostData.R
@@ -6,6 +6,11 @@ data$buffer.lower <- factor(data$buffer.lower)
 data$bitrate <- factor(data$bitrate, levels = c('2','6', '10'))
 data.pareto$bitrate <- factor(data.pareto$bitrate, levels = c('2','6', '10'))
 
+labeller.bitrate <- function(variable, value) {
+  do.call(expression, lapply(levels(value)[value], function(value) {
+    bquote(paste(.(value), ' ', (Mbit^-1)))
+  }))
+}
 
 p <- ggplot(data) +
   geom_line(data = data.pareto, aes(x = wasted.data,
@@ -14,7 +19,7 @@ p <- ggplot(data) +
                  y = power,
                  color = buffer.lower,
                  shape = buffer.size)) +
-  facet_grid(. ~ bitrate, scale = "free") +
+  facet_grid(. ~ bitrate, scale = "free", labeller = labeller.bitrate) +
    labs(x = label.wasted.traffic,
         y = label.energy,
         color = label.buffer.lower,
